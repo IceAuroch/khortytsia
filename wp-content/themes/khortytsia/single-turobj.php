190945a5a6b7fe9206f16d12a13bbcf3554a4607
Oleh Talanoff <talanov.o@gmail.com>
2019-04-10 12:48:44 +0300

    #

--- wp-content/themes/khortytsia/single-turobj.php
+++ wp-content/themes/khortytsia/single-turobj.php
@@ -6,9 +6,9 @@ Template Post Type: tourist
 get_header();
 
 if (have_posts()) :
-    while (have_posts()) : the_post();
+	while (have_posts()) : the_post();
 
-        ?>
+		?>
         <section class="main_desk_section main_desk_turobj_section"
                  style="background-image: url(<?= get_the_post_thumbnail_url(); ?>)">
             <div class="desktop_gradient"></div>
@@ -22,11 +22,11 @@ if (have_posts()) :
                                     <ol class="breadcrumb">
                                         <li class="breadcrumb-item">
                                             <a href="<?= pll_home_url(); ?>">
-                                                <?= __('Головна'); ?>
+												<?= __('Головна'); ?>
                                             </a>
                                         </li>
                                         <li class="breadcrumb-item active" aria-current="page">
-                                            <?= the_title(); ?>
+											<?= the_title(); ?>
                                         </li>
                                     </ol>
                                 </nav>
@@ -36,12 +36,12 @@ if (have_posts()) :
                                     <svg width="30" height="30">
                                         <use xlink:href="<?= get_field('tur_icon', $post->ID); ?>"></use>
                                     </svg>
-                                    <?= the_title(); ?>
+									<?= the_title(); ?>
                                 </h1>
                                 <div class="main_desk_other_text">
                                     <p><?php the_field('tur_annotation'); ?></p>
                                     <a href="#" class="btn btn-secondary" data-toggle="modal" data-target="#form_modal">
-                                        <?= __('Замовити екскурсію'); ?>
+										<?= __('Замовити екскурсію'); ?>
                                     </a>
                                 </div>
                             </div>
@@ -57,9 +57,9 @@ if (have_posts()) :
             <div class="container-fluid">
                 <div class="row">
 
-                    <?php
-                    $images = get_field('tur_obj_gallery', $post->ID);
-                    if (count($images) || have_rows('tur_video')): ?>
+					<?php
+					$images = get_field('tur_obj_gallery', $post->ID);
+					if ($images || have_rows('tur_video')): ?>
                         <div class="col-lg-6 col-xl-5 offset-xl-1">
                             <div class="slider_title">
                                 <div class="slider_title_item">
@@ -74,20 +74,20 @@ if (have_posts()) :
                             <div class="main_slider_wrap">
                                 <div class="slider_main">
 
-                                    <?php
-                                    if (count($images)) :
-                                        foreach ($images as $image): ?>
+									<?php
+									if ($images) :
+										foreach ($images as $image): ?>
                                             <div class="slider_main_item"
                                                  style="background-image: url(<?= $image['url']; ?>);">
                                             </div>
-                                        <?php
-                                        endforeach;
-                                    endif; ?>
+										<?php
+										endforeach;
+									endif; ?>
 
 
-                                    <?php
-                                    if (have_rows('tur_video')):
-                                        while (have_rows('tur_video')) : the_row(); ?>
+									<?php
+									if (have_rows('tur_video')):
+										while (have_rows('tur_video')) : the_row(); ?>
 
                                             <div class="slider_main_item">
                                                 <iframe src="<?= get_video_embed(get_sub_field('tur_video_link')) ?>"
@@ -96,9 +96,9 @@ if (have_posts()) :
                                                         allowfullscreen></iframe>
                                             </div>
 
-                                        <?php
-                                        endwhile;
-                                    endif; ?>
+										<?php
+										endwhile;
+									endif; ?>
 
                                 </div>
                                 <div class="slider_btn slider_btn_prev">
@@ -114,23 +114,23 @@ if (have_posts()) :
                             </div>
                             <div class="slider_main_for">
 
-                                <?php
-                                if (count($images)) :
-                                    foreach ($images as $image): ?>
+								<?php
+								if (count($images)) :
+									foreach ($images as $image): ?>
 
                                         <div class="slider_main_item_for">
                                             <img src="<?= $image['url']; ?>" alt="">
                                         </div>
 
-                                    <?php
-                                    endforeach;
-                                endif;
-                                ?>
+									<?php
+									endforeach;
+								endif;
+								?>
 
 
-                                <?php
-                                if (have_rows('tur_video')):
-                                    while (have_rows('tur_video')) : the_row(); ?>
+								<?php
+								if (have_rows('tur_video')):
+									while (have_rows('tur_video')) : the_row(); ?>
 
                                         <div class="slider_main_item_for">
                                             <iframe src="<?= get_video_embed(get_sub_field('tur_video_link')); ?>"
@@ -139,15 +139,15 @@ if (have_posts()) :
                                                     allowfullscreen></iframe>
                                         </div>
 
-                                    <?php
-                                    endwhile;
-                                endif; ?>
+									<?php
+									endwhile;
+								endif; ?>
 
                             </div>
                         </div>
 
-                    <?php endif;
-                    wp_reset_postdata(); ?>
+					<?php endif;
+					wp_reset_postdata(); ?>
 
                     <div class="col-lg-5 col-xl-5 offset-xl-1">
                         <div class="map_side_text ">
@@ -170,17 +170,17 @@ if (have_posts()) :
                             <h2><?= __('Інші туроб’єкти Хортиці'); ?></h2>
                         </div>
 
-                        <?php $turobj = new WP_Query([
-                            'post_type' => 'tourist',
-                            'orderby' => 'id',
-                            'order' => 'asc'
-                        ]);
-                        if ($turobj->have_posts()):
-                            ?>
+						<?php $turobj = new WP_Query([
+							'post_type' => 'tourist',
+							'orderby' => 'id',
+							'order' => 'asc',
+						]);
+						if ($turobj->have_posts()):
+							?>
 
                             <div class="another_tur_obj_links">
 
-                                <?php while ($turobj->have_posts()): $turobj->the_post(); ?>
+								<?php while ($turobj->have_posts()): $turobj->the_post(); ?>
 
                                     <div class="map_side_icon_item from_bottom">
                                         <a href="<?= the_permalink(); ?>"
@@ -188,17 +188,17 @@ if (have_posts()) :
                                             <svg width="30" height="30">
                                                 <use xlink:href="<?= get_field('tur_icon', $post->ID); ?>"></use>
                                             </svg>
-                                            <?= the_title(); ?>
+											<?= the_title(); ?>
                                         </a>
                                     </div>
 
-                                <?php endwhile; ?>
+								<?php endwhile; ?>
 
                             </div>
 
-                        <?php endif;
-                        wp_reset_postdata();
-                        ?>
+						<?php endif;
+						wp_reset_postdata();
+						?>
 
                     </div>
                 </div>
@@ -210,9 +210,9 @@ if (have_posts()) :
             <div class="map_scrol"><h4><?= __('Нажмите для увеличения'); ?></h4></div>
         </section>
 
-    <?php
+	<?php
 
-    endwhile;
+	endwhile;
 endif;
 require('template-parts/order-excursion.php');
 require('template-parts/last-news.php');
